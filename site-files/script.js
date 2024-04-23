4e3888dc07ecce09adc708d7c642b7be4712e29a
ann1ejohansson <162436676+ann1ejohansson@users.noreply.github.com>
2024-04-23 14:36:14 +0200

    Create script.js

---
+++ site-files/script.js
@@ -0,0 +1,13 @@
+function openModal(imageSrc, captionText) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImage");
+  var caption = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+  caption.innerHTML = captionText;
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
