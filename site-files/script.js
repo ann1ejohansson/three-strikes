f406f01cb01e1b4cdbfc3f960aad4c6783d02d73
Annie Johansson <162436676+ann1ejohansson@users.noreply.github.com>
2024-04-24 17:30:59 +0200

    Update script.js

--- site-files/script.js
+++ site-files/script.js
@@ -14,3 +14,21 @@ function closeModal() {
 
 var IndexValue = 1;
 showImg(IndexValue);
+function side_slide(e) {
+  showImg (indexValue +=e);
+}
+function showImg(e) {
+  var i; 
+  const img = document.querySelectorAll('img')
+  if(e > img.length) {
+    IndexValue = 1
+  }
+  if(e < 1) {
+    IndexValue = img.length
+  }
+  for(i = 0; i < img.length; i++) {
+    img[i].style.display = "none"; 
+  }
+  img[IndexValue - 1].style.display = "block"; 
+    
+    
